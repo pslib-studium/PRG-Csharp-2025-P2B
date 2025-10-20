2fb888f3e826d34f25fe932c95f3cf0e231e3bc1
Tomáš Erlebach <tomas.erlebach@pslib.cz>
2025-10-20 14:26:00 +0200

    pole

---
+++ 05-Pole/Program.cs
@@ -0,0 +1,32 @@
+﻿// =========================================================
+// Pole v C# 
+// =========================================================
+
+// Deklarace a inicializace pole o velikosti 7 s defaultními hodnotami (nuly)
+int[] emptyArr = new int[7];
+
+// Inicializace pole s předdefinovanými hodnotami
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+string[] fruits = { "Jablko", "Banán", "Pomeranč" };
+
+
+Console.WriteLine(numbers[0]); // výpis první hodnoty pole (index 0)
+Console.WriteLine(numbers[3]); // výpis čtvrté hodnoty pole (index 3)
+
+// Změna hodnoty na indexu 3
+numbers[3] = 100;
+Console.WriteLine("hodnota změněna na " + numbers[3]);
+
+// Výpis délky pole
+Console.WriteLine("Délka pole: " + numbers.Length);
+
+// Výpis hodnot pomocí foreach cyklu
+foreach (int num in numbers)
+{
+    Console.Write(num);
+}
+// Výpis hodnot pomocí for cyklu
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine($"numbers[{i}] = {numbers[i]}");
+}
---
+++ 06-PráceSPolem/Program.cs
@@ -0,0 +1,57 @@
+﻿// =========================================================
+// Práce s poli v C#
+// =========================================================
+
+
+// 1. Reference na stejné pole
+int[] arr1 = { 1, 2, 3, 4, 5 };
+int[] arr2 = arr1;        // arr2 odkazuje na stejné pole jako arr1
+arr2[2] = 100;
+Console.WriteLine("Hodnota na indexu 2 v arr1: " + arr1[2]); // Výstup: 100
+
+// 2. Vytvoření kopie pole
+int[] a = { 5, 6, 7, 8, 9 };
+int[] b = (int[])a.Clone();  // vytvoření samostatné kopie pole
+b[2] = 100;
+Console.WriteLine("Hodnota na indexu 2 v a: " + a[2]);      // Výstup: 7
+
+// 3. Řazení a manipulace s polem
+int[] numbers2 = { 3, 1, 6, 2, 4 };
+Array.Sort(numbers2);          // řazení vzestupně
+Array.Reverse(numbers2);       // otočení pořadí prvků
+
+Console.WriteLine("Obsahuje pole hodnotu 6? " + numbers2.Contains(6));
+
+int[] emptyArr = new int[5];
+Array.Fill(emptyArr, 1);       // naplnění pole hodnotou 1
+
+// 4. Dvourozměrné pole 
+int[,] map = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+Console.WriteLine("Výpis hodnot matice:");
+foreach (int x in map)
+{
+    Console.Write(x);
+}
+
+Console.WriteLine(" Počet dimenzí matice: " + map.Rank);
+
+
+// 5. Zubaté pole (jagged array)
+int[][] jArray =
+[
+    [1, 3, 5, 7, 9],
+    [0, 2, 4, 6],
+    [11, 22],
+];
+
+jArray[0][1] = 77;  // změna hodnoty
+
+Console.WriteLine("Výpis hodnot zubatého pole:");
+foreach (int[] row in jArray)
+{
+    foreach (int value in row)
+    {
+        Console.Write(value + " ");
+    }
+}
+Console.WriteLine();
